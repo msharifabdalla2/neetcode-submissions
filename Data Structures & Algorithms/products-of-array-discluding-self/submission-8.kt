27eb204eddb3408669bbd4f32f649927eb0af222
Mohamed Abdalla <msharifabdalla1@yahoo.com>
2026-06-30 20:27:03 +0100

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+
+        val resultArray = MutableList<Int>(nums.size) { 0 }
+
+        var preFix: Int = 1
+        var postFix: Int = 1
+
+        for (i in nums.indices) {
+            resultArray[i] = preFix
+
+            preFix *= nums[i]
+        }
+
+        for (i in nums.indices.last downTo 0) {
+
+            resultArray[i] *= postFix
+
+            postFix *= nums[i]
+        }
+
+        return resultArray.toIntArray()
+    }
+}

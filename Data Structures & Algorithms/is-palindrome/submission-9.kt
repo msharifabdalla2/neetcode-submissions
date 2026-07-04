fc9437158b2e84bfe36eabcc3c177da0399b15e4
Mohamed Abdalla <msharifabdalla1@yahoo.com>
2026-07-04 22:48:41 +0100

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+
+        val cleanedWord: String = s.filter { it.isLetterOrDigit() }.lowercase()
+
+        var leftPointer: Int = 0
+        var rightPointer: Int = cleanedWord.lastIndex
+
+        while (leftPointer <= rightPointer){
+            if (cleanedWord[leftPointer] !== cleanedWord[rightPointer]) return false
+
+            leftPointer++
+            rightPointer--
+        }
+
+        return true
+    }
+}

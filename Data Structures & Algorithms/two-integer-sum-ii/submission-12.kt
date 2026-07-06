c1ddb93e9e4fbeaf11dcee48b91e15ca60e8b793
Mohamed Abdalla <msharifabdalla1@yahoo.com>
2026-07-06 10:37:31 +0100

    Add: two-integer-sum-ii - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-12.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+
+        var leftPointer: Int = 0
+        var rightPointer: Int = numbers.lastIndex
+
+        while (leftPointer < rightPointer) {
+
+            var total: Int = numbers[leftPointer] + numbers[rightPointer]
+
+            if (total == target) {
+                return mutableListOf(leftPointer +1, rightPointer + 1).toIntArray()
+            }
+
+            else if (total < target) leftPointer++
+
+            else if (total > target) rightPointer--
+        }
+
+        return mutableListOf<Int>().toIntArray()
+    }
+}

16bd74f5106f3135ca6266945a24999bf1f8ade0
Mohamed Abdalla <msharifabdalla1@yahoo.com>
2026-07-10 11:20:16 +0100

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.kt
@@ -0,0 +1,33 @@
+class Solution {
+    fun evalRPN(tokens: Array<String>): Int {
+
+        val stack = ArrayDeque<Int>()
+
+        for (token in tokens) {
+            when (token) {
+                "+" -> {
+                    val pop1 = stack.removeLast()
+                    val pop2 = stack.removeLast()
+                    stack.addLast(pop1 + pop2)
+                }
+                "*" -> {
+                    val pop1 = stack.removeLast()
+                    val pop2 = stack.removeLast()
+                    stack.addLast(pop1 * pop2)
+                }
+                "-" -> {
+                    val pop1 = stack.removeLast()
+                    val pop2 = stack.removeLast()
+                    stack.addLast(pop2 - pop1)
+                }
+                "/" -> {
+                    val pop1 = stack.removeLast()
+                    val pop2 = stack.removeLast()
+                    stack.addLast(pop2 / pop1)
+                }
+                else -> stack.addLast(token.toInt())
+            }
+        }
+        return stack.last()
+    }
+}

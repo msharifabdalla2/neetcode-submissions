d74c543353477e51655cb81069bd0e3997e2a85a
Mohamed Abdalla <msharifabdalla1@yahoo.com>
2026-07-17 16:26:36 +0100

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+
+        var lP: Int = 0
+        var rP: Int = 1
+        var maxProfit: Int = 0
+
+        while (rP < prices.size) {
+            if (prices[rP] > prices[lP]) {
+                val profit: Int = prices[rP] - prices[lP]
+                maxProfit = maxOf(profit, maxProfit)
+            }
+            else lP = rP
+
+            rP++
+        }
+        return maxProfit
+    }
+}

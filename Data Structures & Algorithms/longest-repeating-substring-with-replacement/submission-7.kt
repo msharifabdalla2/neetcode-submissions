44a4726264a8b61a4ea10b73ead52d7de738f708
Mohamed Abdalla <msharifabdalla1@yahoo.com>
2026-07-23 19:38:58 +0100

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+
+        var lP: Int = 0
+        val map = hashMapOf<Char, Int>()
+        var maxLen: Int = 0
+
+        for (rP in s.indices) {
+            map[s[rP]] = map.getOrDefault(s[rP], 0) + 1
+
+            while (((rP - lP) + 1) - (map.values.maxOrNull() ?: 0) > k) {
+                map[s[lP]] = map.getOrDefault(s[lP], 0) - 1
+                lP++
+            }
+
+            maxLen = maxOf(maxLen, (rP - lP) + 1)
+        }
+        return maxLen
+    }
+}

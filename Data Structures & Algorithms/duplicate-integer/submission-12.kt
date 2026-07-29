6b8c4b1fc1f78a06413648df9f4de48412c97eb8
Mohamed Abdalla <msharifabdalla1@yahoo.com>
2026-07-29 17:15:55 +0100

    Add: duplicate-integer - submission-12

---
+++ Data Structures & Algorithms/duplicate-integer/submission-12.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun hasDuplicate(nums: IntArray): Boolean {
+
+        val set = mutableSetOf<Int>()
+
+        for (num in nums) {
+            if (num in set) {
+                return true
+            }
+
+            else set.add(num)
+        }
+
+        return false
+    }
+}
